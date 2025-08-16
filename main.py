acc0f1bfe4d5a225aadebd35c755c044b80a5d88
Divyam Deep <92738353+Divyam-Deep@users.noreply.github.com>
2025-08-17 00:43:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader
-from langchain.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
